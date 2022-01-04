e2ef0905253ddb5e4f0158693d52f64cb90ba050
Khalil <khalilr432@gmail.com>
2022-01-04 16:08:04 -0500

    almost done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  let [fetched_students,Setfetched_students] = useState([])
+  let [loaded,Setloaded] = useState(false)
+
+  function Checkdata(data){
+    let answer = false
+   try{
+     let num = data[0]
+    answer = true
+   }catch{
+     setTimeout(Checkdata(), 2000)
+   }
+   return answer
+  }
+  
+  useEffect(() => {
+    async function Setdata(){
+
+      await axios.get('https://api.hatchways.io/assessment/students').then(response => response.data.students).then(data => Setfetched_students(data))
+      
+    }
+    Setdata()
+    if(typeof fetched_students[0] === 'undefined' ){
+      Setloaded(false)
+    }else{
+      Setloaded(true)
+    }
+    return () => {
+    
+    }
+  },)
+
+  if(loaded){
+    return(
+      <div>
+        <p>done loading</p>
+        <p>{console.log(fetched_students[0].city)}</p>
+      </div>
+    )
+  }else{
+    return(
+      <div>
+        <p>... waiting to recieve data</p>
+      </div>
+    )
+  }
+  }
+ 
+ 
+  
+ 
 
 export default App;

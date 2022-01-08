e46086b98e800dfcfca164e57b1e1cf12d265f6b
Khalil <khalilr432@gmail.com>
2022-01-08 18:05:08 -0500

    extra stuff

--- src/Datatable.js
+++ src/Datatable.js
@@ -1,6 +1,9 @@
 import './index.css'
+import plus from './plus.png'
+import React, {useState} from 'react';
 
 export default function Datatable({data}){
+    let [viewgrades,Setviewgrades] = useState(false)
     function Getaverage(array){
         var total = 0
       
@@ -30,7 +33,10 @@ export default function Datatable({data}){
                 <p className='info'>Skill: {students.skill}</p>
                 <br></br>
                <p className='info'>Average: {Getaverage(students.grades)}</p>
-           
+               <></>
+        </div>
+        <div>
+       <input type='image' src={plus} alt='grades' width="10" height = "10" onClick={Setviewgrades(!viewgrades)}/>
         </div>
       </>
       )}
